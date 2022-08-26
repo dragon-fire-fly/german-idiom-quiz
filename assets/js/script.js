3e2ede1ca615b7f19537b20149eccade58b9d3a4
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-26 09:29:55 +0000

    Add const variables and link together

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.getElementById("idiom");
+const choices = document.getElementsByClassName("option")
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
