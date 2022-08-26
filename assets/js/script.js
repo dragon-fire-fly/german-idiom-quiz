cdf3e9c776443e5f4c42f226bb1b59cff92823fe
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-26 12:12:23 +0000

    Assign variables and constants

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,63 @@ const choices = document.getElementsByClassName("option")
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions =[];
+
+
+const POINT_VALUE = 1
+const TOTAL_QUESTIONS = 12
+
+
+/* Example Qs */
+let questions = [
+    {
+        id : 1,
+        idiom : "Der Koch war verliebt",
+        literal_translation : "The cook was in love",
+        meaning1 : "To get an extra portion",
+        meaning2 : "The meal is too salty",
+        meaning3 : "To find a hair in your meal",
+        meaning4 : "The meal is already cold",
+        correct_meaning : "The meal is too salty",
+        english_equivalent: ""
+    },
+    {
+        id : 2,
+        idiom : "Am Essen war die Küchenfee dran",
+        literal_translation : "It was the Kitchen Fairy's turn to cook",
+        meaning1 : "To get a take-away",
+        meaning2 : "",
+        meaning3 : "",
+        meaning4 : "The food was burnt",
+        correct_meaning : "The food was burnt",
+        english_equivalent: "",
+    },
+    {
+        id : 3,
+        idiom : "Nicht die hellster Glühbirne (im Shrank)",
+        literal_translation : "Not the brightest lightbulb (in the cupboard)",
+        meaning1 : "Someone who is overly critical of others",
+        meaning2 : "Someone who is not considered very attractive",
+        meaning3 : "Someone who is not very welcoming to others",
+        meaning4 : "Someone who is not considered very smart",
+        correct_meaning : "Someone who is not considered very smart",
+        english_equivalent : "Not the sharpest tool in the shed",
+    },
+    {
+        id : 4,
+        idiom : "Das ist nur ein Katzensprung ",
+        literal_translation : "That is only a cat's leap",
+        meaning1 : "It is right around the corner",
+        meaning2 : "",
+        meaning3 : "",
+        meaning4 : "",
+        correct_meaning : "It is right around the corner",
+        english_equivalent : "Only a stone's throw",
+    }
+]
